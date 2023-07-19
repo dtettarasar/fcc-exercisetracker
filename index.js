89fb42d259b829b0e77851f57c8236bb6d21acb0
Replit user <>
2023-07-19 11:00:33 +0000

    built database connexion

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+// init database connexion
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(process.env.DB_URL);
+const db = client.db('fcc-exercice-tracker');
+const users = db.collection('users');
+const exercices = db.collection('exerciceDoc');
+
+/*
+// Database infos
+
+console.log(db);
+console.log("-------");
+console.log(users);
+console.log("-------");
+console.log(exercices);
+*/
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
